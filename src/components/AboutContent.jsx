4c794959a68a23a69d8ac4334b87f31d0f18c15b
Vinicius Simão de Melo <90805960+viniciusimao@users.noreply.github.com>
2023-05-10 10:54:00 -0300

    Update AboutContent.jsx

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -6,10 +6,10 @@ const AboutContent = () => {
     <section className="about-container">
       <h2>Sobre</h2>
       <p>
-        Olá, Me chamo Vinicius mas pode me chamar só de Simão, tenho 21 anos e sou apaixonado no mundo da tecnologia e por pizza.
+        Olá, Me chamo Vinicius mas pode me chamar só de Simão, tenho 22 anos e sou apaixonado no mundo da tecnologia e por pizza.
       </p>
       <p>
-        Sou recem formado em Desevolvimento fullStack pela <a href="https://recodepro.org.br/">Recode Pro </a> e tenho treinamento tanto em softskills e hardskills.
+        Sou formado em Desevolvimento fullStack pela <a href="https://recodepro.org.br/">Recode Pro </a> e tenho treinamento tanto em softskills e hardskills.
       </p>
     </section>
   )
